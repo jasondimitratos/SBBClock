4ef1cf3647c2de4d12e6fde9c26ae26ad093e697
Jason Dimitratos <jason.dimitratos@students.fhnw.ch>
2016-05-05 14:25:18 +0200

    Secunden und Minutenzeiger version1

--- Java_Projekt/src/main/java/SBBClock/view/Minutenzeiger.java
+++ Java_Projekt/src/main/java/SBBClock/view/Minutenzeiger.java
@@ -1,7 +1,42 @@
 package SBBClock.view;
 
+import SBBClock.model.WatchPM;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
 /**
  * Created by andreazirn on 05/05/16.
  */
-public class Minutenzeiger {
+public class Minutenzeiger extends HBox {
+    private final WatchPM model;
+    private Circle minutes;
+    private Line minutenzeiger;
+    public Minutenzeiger(WatchPM model) {
+        this.model = model;
+        initializeControls();
+        layoutControls();
+        addBindings();
+    }
+
+
+    private void initializeControls() {
+        minutes= new Circle(150,150,130);
+        minutenzeiger= new Line(150,175,150,20);
+    }
+
+    private void layoutControls() {
+        getChildren().addAll(minutenzeiger,minutes);
+    }
+
+    private void addEventHandlers() {
+    }
+
+    private void addValueChangedListeners() {
+    }
+
+    private void addBindings() {
+
+    }
+
 }
--- Java_Projekt/src/main/java/SBBClock/view/Sekundenzeiger.java
+++ Java_Projekt/src/main/java/SBBClock/view/Sekundenzeiger.java
@@ -1,7 +1,41 @@
 package SBBClock.view;
 
+import SBBClock.model.WatchPM;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
 /**
  * Created by andreazirn on 05/05/16.
  */
-public class Sekundenzeiger {
+public class Sekundenzeiger extends HBox {
+    private final WatchPM model;
+    private Circle seconds;
+    private Line sekundenzeiger;
+    public Sekundenzeiger(WatchPM model) {
+        this.model = model;
+        initializeControls();
+        layoutControls();
+        addBindings();
+    }
+
+
+    private void initializeControls() {
+        seconds= new Circle(150,150,130);
+        sekundenzeiger= new Line(150,175,150,20);
+    }
+
+    private void layoutControls() {
+        getChildren().addAll(seconds,sekundenzeiger);
+    }
+
+    private void addEventHandlers() {
+    }
+
+    private void addValueChangedListeners() {
+    }
+
+    private void addBindings() {
+
+    }
 }
