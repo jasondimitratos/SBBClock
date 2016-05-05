6386f9f0dbb85c4d96acb53163e493218572f217
Jason Dimitratos <jason.dimitratos@students.fhnw.ch>
2016-05-05 14:27:47 +0200

    stundenzeiger version1

--- Java_Projekt/src/main/java/SBBClock/view/Stundenzeiger.java
+++ Java_Projekt/src/main/java/SBBClock/view/Stundenzeiger.java
@@ -1,7 +1,41 @@
 package SBBClock.view;
 
+import SBBClock.model.WatchPM;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Line;
+
 /**
  * Created by andreazirn on 05/05/16.
  */
-public class Stundenzeiger {
+public class Stundenzeiger extends HBox {
+    private final WatchPM model;
+    private Circle stunden;
+    private Line stundenzeiger;
+    public Stundenzeiger(WatchPM model) {
+        this.model = model;
+        initializeControls();
+        layoutControls();
+        addBindings();
+    }
+
+
+    private void initializeControls() {
+        stunden= new Circle(150,150,130);
+        stundenzeiger= new Line(150,175,150,20);
+    }
+
+    private void layoutControls() {
+        getChildren().addAll(stundenzeiger,stunden);
+    }
+
+    private void addEventHandlers() {
+    }
+
+    private void addValueChangedListeners() {
+    }
+
+    private void addBindings() {
+
+    }
 }
